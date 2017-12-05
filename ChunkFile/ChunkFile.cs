02a06059f2d013cdc5e30a5dd45ad87462a838d4
Meng Chen (Design Laboratory Inc) <v-cheme@microsoft.com>
2017-12-05 15:52:48 -0800

    initial commit

---
+++ ChunkFile/ChunkFile.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunkFile
+{
+    static class ChunkFile
+    {
+        public void chunkFile(string sourceFileFullName, int chunkSize)
+        {
+
+            int chunkRowCount = 0;
+            int chunk = 1;
+            string inputFileName = Path.GetFileNameWithoutExtension(sourceFileFullName);
+            string extension = Path.GetExtension(sourceFileFullName);
+
+            string outputFileName;
+
+            if (!File.Exists(sourceFileFullName))
+            {
+                Console.WriteLine("Invalid input file.");
+            }
+
+            StreamWriter outPutFileWriter;
+
+            using (StreamReader sr = new StreamReader(sourceFileFullName))
+            {
+                string line = null;
+
+                outputFileName = String.Format("inputFileName_{0}.{1}", chunk, extension);
+                outPutFileWriter = new StreamWriter(outputFileName);
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    chunkRowCount++;
+                   
+
+                    if (chunkRowCount == chunkSize)
+                    {
+                        chunkRowCount = 0;
+                        yield return chunkDataTable;
+                        chunkDataTable = null;
+                    }
+                }
+            }
+            ////return last set of data which less then chunk size
+            //if (null != chunkDataTable)
+            //    yield return chunkDataTable;
+        }
+    }
+}
---
+++ ChunkFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChunkFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please input your file path:");
+            string inputFilePath = Console.ReadLine();
+
+            Console.WriteLine("Please input your chunk size in a number");
+            string inputSize = Console.ReadLine();
+            int chunkSize;
+            while (!Int32.TryParse(inputSize, out chunkSize))
+            {
+                Console.WriteLine("Please input an integer number.");
+                inputSize = Console.ReadLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
